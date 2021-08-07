6a5d3adfb04bdc8a6ceac935a865ca2f9b325707
Mohammad Hossein Komeili <mohammad.h.komeili1377@gmail.com>
2021-08-07 15:52:30 +0430

    day 1 project of 100DaysofCode added.

---
+++ Day1/bandNameGenerator.py
@@ -0,0 +1,8 @@
+# Greeting
+print("Hi there, I am a Band Name Generator. answer two question to get your fancy name :)")
+
+city = input('What is the name of the city you born?:\n')
+
+pet = input('What is the name of your pet?:\n')
+
+print('Here is your cool band name: {} {}'.format(city, pet))
