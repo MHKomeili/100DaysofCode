51c457293dddb38fe7de64e236f349fccf895bb8
Mohammad <mohammad.h.komeili.1377@gmail.com>
2021-09-01 00:16:07 +0430

    day 20 project of 100 days of code added. First part of Snake game covered today

---
+++ Day20_21/food.py
@@ -0,0 +1,7 @@
+import time
+from turtle import Turtle, Screen
+import random
+
+class Food:
+    def __init__(self):
+        pass
---
+++ Day20_21/snake.py
@@ -0,0 +1,48 @@
+import time
+from turtle import Turtle, Screen
+
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        x_pos = 0
+        for i in range(3):
+            new_part = Turtle(shape="square")
+            new_part.color("white")
+            new_part.penup()
+            new_part.setposition(x=x_pos, y=0)
+            x_pos -= 20
+            self.segments.append(new_part)
+
+    def move(self):
+        for i in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[i - 1].xcor()
+            new_y = self.segments[i - 1].ycor()
+            self.segments[i].setposition(x=new_x, y=new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
