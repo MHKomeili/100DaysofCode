4513a0f480904a49f02c17dfc24aeedab0255567
Mohammad <mohammad.h.komeili.1377@gmail.com>
2021-09-05 18:37:34 +0430

    day 25 project of 100 days of code added. US States Quiz.

---
+++ Day25/main.py
@@ -0,0 +1,36 @@
+import turtle
+import pandas as pd
+FONT = ("Courier", 10, "normal")
+
+
+writer = turtle.Turtle()
+writer.hideturtle()
+writer.penup()
+
+screen = turtle.Screen()
+screen.title("U.S States Quiz")
+screen.setup(width=725, height=491)
+screen.bgpic("blank_states_img.gif")
+
+data = pd.read_csv("50_states.csv")
+states = data.state.to_list()
+guessed_states = []
+while len(guessed_states) < 50:
+    guess = screen.textinput(title=f"{len(guessed_states)}/50 States correct", prompt="What is another State's name? ").title()
+
+    if guess == "Exit":
+        missing_states = []
+        for s in states:
+            if s not in guessed_states:
+                missing_states.append(s)
+        pd.DataFrame(missing_states).to_csv("states_to_learn.csv")
+        break
+
+    if guess in states:
+        x = int(data[data["state"] == guess].x)
+        y = int(data[data["state"] == guess].y)
+        writer.goto(x, y)
+        writer.write(f"{guess}", align="center", font=FONT)
+        guessed_states.append(guess)
+
+screen.exitonclick()
